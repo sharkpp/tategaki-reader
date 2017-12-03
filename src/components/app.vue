ef706533bef42cca2f7afc5e444f20c4b2243abd
sharkpp <touroku@sharkpp.net>
2017-12-03 21:58:37 +0900

    test

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,6 +1,11 @@
 <style>
-  .red {
-    color: #f00;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html, body {
+    min-height: 100%;
   }
   .no-select {
     user-select: none; /* CSS3 */
@@ -8,36 +13,94 @@
     -webkit-user-select: none; /* Safari、Chromeなど */
     -ms-user-select: none; /* IE10かららしい */
   }
+  #content {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    position: absolute;
+  }
   .vertical {
     width: 100%;
     height: 100%;
+    padding: 8px;
+    position: relative;
     overflow: hidden;
     font-family: 'Yu Mincho', YuMincho, 'Hiragino Mincho ProN', 'Hiragino Mincho Pro', 'HGP明朝B', serif;
     -webkit-font-feature-settings: 'pkna';
             font-feature-settings: 'pkna';
-    font-size: 17px;
-    line-height: 1.9;
-    letter-spacing: 0.03em;
+    font-size: 16px;
+    line-height: 20px;
+    letter-spacing: 1px;
     -webkit-writing-mode: vertical-rl;
         -ms-writing-mode: tb-rl;
             writing-mode: vertical-rl;
   }
-  .vertical > p {
+  .vertical > div {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    position: relative;
+  }
+  .vertical > div > p {
     position: absolute;
     right: 0;
-  }
-  .vertical p {
-    text-indent: 1em;
-    margin-left: 30px;
   }
   .vertical ruby {
     font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Yu Gothic", YuGothic, Verdana, Meiryo, "M+ 1p", sans-serif;
   }
+  #touch-area {
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    position: absolute;
+  }
+  #touch-settings {
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 15%;
+    position: absolute;
+  }
+  #touch-settings + div {
+    top: 15%;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 85%;
+    position: absolute;
+  }
+  #touch-page-next {
+    top: 0;
+    left: 0;
+    width: 50%;
+    height: 100%;
+    position: absolute;
+  }
+  #touch-page-prev {
+    top: 0;
+    right: 0;
+    width: 50%;
+    height: 100%;
+    position: absolute;
+  }
 </style>
 
 <template>
-  <div id="page" class="vertical" v-on:click="nextPage">
-    <p class="no-select">{{ result }}</p>
+  <div id="content" >
+    <div id="page" class="vertical" >
+      <div>
+        <p class="no-select" v-html="result">{{ result }}</p>
+      </div>
+    </div>
+    <div id="touch-area">
+      <div id="touch-settings"></div>
+      <div>
+        <div id="touch-page-next" v-on:click="nextPage"></div>
+        <div id="touch-page-prev" v-on:click="prevPage"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -50,10 +113,24 @@
       testMethod: () => {
         console.log('Component Test')
       },
-      nextPage: () => {
-        const pageContainer = document.getElementById('page');
+      prevPage: (e) => {
+console.log('prevPage',e);
+        const pageContainer = document.getElementById('page').firstChild;
+        const pageContents  = pageContainer.firstChild;
+        const pageWidth = parseInt(pageContainer.clientWidth / 20) * 20;
         //
-        pageContainer.firstChild.style.right = (+pageContainer.firstChild.style.right - pageContainer.clientWidth) + 'px';
+        const curOffset = parseInt(pageContents.style.right) || 0;
+        pageContents.style.right = Math.min(curOffset + pageWidth, 0) + 'px';
+        console.log([pageContainer]);
+      },
+      nextPage: (e) => {
+console.log('nextPage',e);
+        const pageContainer = document.getElementById('page').firstChild;
+        const pageContents  = pageContainer.firstChild;
+        const pageWidth = parseInt(pageContainer.clientWidth / 20) * 20;
+        //
+        const curOffset = parseInt(pageContents.style.right) || 0;
+        pageContents.style.right = Math.max(curOffset - pageWidth, -parseInt((pageContents.clientWidth - pageWidth) / 20) * 20) + 'px';
         console.log([pageContainer]);
       }
     },
